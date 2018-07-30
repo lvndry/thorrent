d7c830af26bd28a860512e4d1e5d78c7f697a69c
lvndry <monga.4landry@gmail.com>
2018-07-30 21:51:13 +0200

    example magnet

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -18,16 +18,12 @@ export default class Home extends Component {
 	componentDidMount() {
 		// getMovies()
 		// .then(movies => this.setState({ movies: movies.results }))
+		var magnetURI = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
 
-		var magnetURI = 'magnet:?xt=urn:btih:5576b680bccc56ee5e3e937fbe5016f52f081931&tr=udp://eddie4.nl:6969/announce&tr=udp://shadowshq.yi.org:6969/announce&tr=udp://tracker.leechers-paradise.org:6969/announce&tr=udp://tracker.pirateparty.gr:6969/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://tracker.coppersurfer.tk:80/announce&tr=udp://9.rarbg.com:2800/announce&tr=udp://9.rarbg.me:2780/announce&tr=udp://9.rarbg.to:2710/announce&tr=udp://p4p.arenabg.com:1337/announce&tr=udp://public.popcorn-tracker.org:6969/announce&tr=udp://tracker.vanitycore.co:6969/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.zer0day.to:1337/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce&tr=udp://ipv4.tracker.harry.lu:80/announce&tr=udp://explodie.org:6969/announce&tr=http://inferno.demonoid.ph:3389/announce'
-
-		client.add(magnetURI, function(torrent) {
-		  // Got torrent metadata!
+		client.add(magnetURI, (torrent) => {
 		  console.log('Client is downloading:', torrent.infoHash)
 			console.log(torrent);
 		  torrent.files.forEach(function(file) {
-		    // Display the file by appending it to the DOM. Supports video, audio, images, and
-		    // more. Specify a container element (CSS selector or reference to DOM node).
 				console.log(file);
 		    file.appendTo('#root')
 		  })
